62a50fc6dd0640bae003f6bb0045f9a72c786f49
KRLEESEUNGJAE <karmaup0711@gmail.com>
2021-10-11 23:09:07 +0900

    make organizer

--- photo/app.js
+++ photo/app.js
@@ -1,5 +1,75 @@
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
 // Todo 1: 사용자가 원하는 폴더의 이름을 받아온다
-// Todo 2: 그 폴더안에 video, captured, duplicated 폴더를 만든다
-// Todo 3: 폴더안에 있는 파일들을 다 돌면서 해당하는 mp4|mov, png|aae, IMG_1234 (IMG_E1234)
 
-console.log(process.argv)
+// console.log(process.argv[2])
+const folder = process.argv[2];
+const workingDir = path.join(os.homedir(), 'Desktop/Development/organizer/photo', folder);
+// console.log(workingDir);
+
+if (!folder || !fs.existsSync(workingDir)) {
+    console.log('Plese enter folder name in Pictures!')
+    return;
+}
+console.log(workingDir);
+
+// Todo 2: 그 폴더안에 video, captured, duplicated 폴더를 만든다
+const videoDir = path.join(workingDir, 'video');
+const capturedDir = path.join(workingDir, 'captured');
+const duplicatedDir = path.join(workingDir, 'duplicated');
+
+!fs.existsSync(videoDir) && fs.mkdirSync(videoDir);
+!fs.existsSync(capturedDir) && fs.mkdirSync(capturedDir);
+!fs.existsSync(duplicatedDir) && fs.mkdirSync(duplicatedDir);
+
+// Todo 3: 폴더안에 있는 파일들을 다 돌면서 해당하는 mp4|mov, png|aae, IMG_1234 (IMG_E1234)
+fs.promises
+    .readdir(workingDir)
+    .then(processFiles)
+    .catch(console.log);
+
+function processFiles(files) {
+    files.forEach((file) => {
+        if (isVideoFile(file)) {
+            move(file, videoDir);
+        } else if (isCapturedFile(file)) {
+            move(file, capturedDir);
+        } else if (isDuplicatedFile(files, file)) {
+            move(file, duplicatedDir);
+        }
+    });
+}
+
+
+function isVideoFile(file) {
+    const regExp = /(mp4|mov)$/gm;
+    const match = file.match(regExp);
+    return !!match;
+}
+
+function isCapturedFile(file) {
+    const regExp = /(png|aae)$/gm;
+    const match = file.match(regExp);
+    return !!match;
+}
+
+function isDuplicatedFile(files, file) {
+    // IMG_XXXX  -> IMG_EXXX
+    if (!file.startsWith('IMG_') || file.startsWith('IMG_E')) {
+        return false;
+    }
+
+    const edited = `IMG_E${file.split('_')[1]}`;
+    const found = files.find((f) => f.includes(edited));
+    return !!found;
+}
+
+function move(file, targetDir) {
+    console.info(`move ${file} to ${path.basename(targetDir)}`);
+    const oldPath = path.join(workingDir, file);
+    const newPath = path.join(targetDir, file);
+    fs.promises
+        .rename(oldPath, newPath) //
+        .catch(console.error);
+}
